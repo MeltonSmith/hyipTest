8b70df3cfaefcbc98eab2f479472a41d398fc9b2
Melton Smith <ravenglc1@gmail.com>
2021-08-02 00:29:03 +0300

    Properties for topics and clean up

--- src/main/java/r/ian/ianlabtest/listener/MessageListener.java
+++ src/main/java/r/ian/ianlabtest/listener/MessageListener.java
@@ -4,10 +4,9 @@ package r.ian.ianlabtest.listener;
  * @author Melton Smith
  * @since 21.07.2021
  *
- * Опциональный интерфейс для лисенеров.
- * Необязательно реализовывать всю инфраструктуру по регистрации и обработке, достаточно и тестов.
+ * Listener optional interface.
  *
- * @param <T> тип сообщений, которые будем слушать.
+ * @param <T> message type to listen.
  */
 public interface MessageListener<T> {
     void listen(T incomingMessage);
